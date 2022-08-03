281f9efc45a05581ce439427902557ac012e1439
Eddy Mena <developer.eddymena@gmail.com>
2022-08-02 19:34:51 -0600

    home login screens +bootstrap custom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { LoginScreenComponent } from './Components/login-screen/login-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, LoginScreenComponent]
