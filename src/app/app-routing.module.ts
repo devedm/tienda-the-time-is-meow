df136c31d69cb76925c7884caee8a3662642bbaf
Carlos M. Leiton Ugalde <m.ugalde99@outlook.com>
2022-08-07 22:39:13 -0600

    Componente Sign Up
    Incorporación de Nuevo componente de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginScreenComponent } from './Components/login-screen/login-screen.component';
 import { PanelComponent } from './Components/panel/panel.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginScreenComponent},
-  { path: 'panel', component: PanelComponent}
+  { path: 'panel', component: PanelComponent},
+  { path: 'signUp', component: SignUpComponent}
 ];
 
 @NgModule({
