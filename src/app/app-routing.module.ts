3bb6f01911faf68fbcacb5c4699f571a04b23e66
Eddy Mena <developer.eddymena@gmail.com>
2022-08-21 20:16:26 -0600

    completamos component tienda, arreglamos color nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './Components/home/home.component';
 import { LoginScreenComponent } from './Components/login-screen/login-screen.component';
 import { PanelComponent } from './Components/panel/panel.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { TiendaComponent } from './Components/tienda/tienda.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginScreenComponent},
   { path: 'panel', component: PanelComponent},
-  { path: 'signUp', component: SignUpComponent}
+  { path: 'signUp', component: SignUpComponent},
+  { path: 'tienda', component: TiendaComponent}
 ];
 
 @NgModule({
@@ -18,4 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, LoginScreenComponent, PanelComponent, SignUpComponent]
+export const routingComponents = [HomeComponent, LoginScreenComponent, PanelComponent, SignUpComponent, TiendaComponent]
