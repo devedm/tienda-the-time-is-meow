f30a23f5f540265fb2dc599a2075cd3610e33798
Eddy Mena <developer.eddymena@gmail.com>
2022-08-23 22:39:28 -0600

    store working

--- src/app/Components/tienda/tienda.component.ts
+++ src/app/Components/tienda/tienda.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
+import { StoreConectionService } from 'src/app/Services/store-conection.service';
 @Component({
   selector: 'app-tienda',
   templateUrl: './tienda.component.html',
   styleUrls: ['./tienda.component.css']
 })
 export class TiendaComponent implements OnInit {
-
-  constructor() { }
+  allUsers: any;
+  currentFilter: any;
+  constructor(
+    public storeConectionService: StoreConectionService
+  ) { }
 
   ngOnInit(): void {
+    this.getUsers()
+  }
+  async getUsers() {
+    this.allUsers = await this.storeConectionService.getAllItems();
+    this.currentFilter = [];
+    for (let item of this.allUsers) {
+      this.currentFilter.push(item)
+    }
+    return this.allUsers
   }
 
+  isPet(element: any) {
+    return (element.isPet == true);
+  }
+  isAccessory(element: any) {
+    return (element.isPet != true);
+  }
+
+  getItems(filter: String) {
+    if (filter == 'pets') {
+      this.currentFilter = []
+      for (let item of this.allUsers.filter(this.isPet)) {
+        this.currentFilter.push(item)
+      }
+    } else {
+      this.currentFilter = [];
+      for (let item of this.allUsers.filter(this.isAccessory)) {
+        this.currentFilter.push(item)
+      }
+    }
+  }
 }
--- src/app/Services/store-conection.service.ts
+++ src/app/Services/store-conection.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreConectionService {
+  constructor(
+    private db: AngularFirestore
+  ) { }
+  getAllItems() {
+    return new Promise<any>((resolve) => {
+      this.db.collection('ttimShopDB').valueChanges({ idField: 'id' }).subscribe(users => resolve(users));
+    })
+  }
 
-  constructor() { }
 }
